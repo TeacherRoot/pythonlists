af39cb7bf7e4edaa02638c775e9765608b63430f
Ann Root <ann.root@bvsd.org>
2024-11-03 18:54:09 -0700

    Create liststart.py

---
+++ liststart.py
@@ -0,0 +1,96 @@
+list1 = [-99,1,2,3,4,5,6,7,8,9,10,12345]
+list2 = [10,9,8,7,6,5,4,3,2,1,-99]
+list3 = [10,20,30,40,50,-11818,40,30,20,10]
+list4 = [9,10,-88,100,-555,1000]
+list5 = [10,10,10,11,456]
+list6 = [-111,1,2,3,9,11,20,30]
+list7 = [9,8,7,6,5,4,3,2,0,-2,-989]
+list8 = [12,15,18,21,23,1000]
+list9 = [250,19,17,15,13,11,10,9,6,3,2,1,-455]
+list10 = [9,10,-8,10000,-5000,1000]
+
+def sumlist( list ):
+    pass
+
+def sumlistfirst( list, value) :
+    pass
+
+#  Write a program that will search through a list to see if the last value in the list occurs more than once.  
+#  If the last value occurs more than once, return true.  Otherwise, return false.  If the list is empty, return false.
+def lastRepeats( list) :
+  pass
+
+def least ( list ):
+  pass
+
+def averageDropLow( list ):
+  pass
+
+print("Sum list")
+print(sumlist(list1))
+print(sumlist(list2))
+print(sumlist(list3))
+print(sumlist(list4))
+print(sumlist(list5))
+print(sumlist(list6))
+print(sumlist(list7))
+print(sumlist(list8))
+print(sumlist(list9))
+print(sumlist(list10))
+print("\n\n")
+print("Sum list First")
+print(sumlistfirst(list1))
+print(sumlistfirst(list2))
+print(sumlistfirst(list3))
+print(sumlistfirst(list4))
+print(sumlistfirst(list5))
+print(sumlistfirst(list6))
+print(sumlistfirst(list7))
+print(sumlistfirst(list8))
+print(sumlistfirst(list9))
+print(sumlistfirst(list10))
+print("\n\n")
+print("Last Repeats")
+print(lastRepeats(list1))
+print(lastRepeats(list2))
+print(lastRepeats(list3))
+print(lastRepeats(list4))
+print(lastRepeats(list5))
+print(lastRepeats(list6))
+print(lastRepeats(list7))
+print(lastRepeats(list8))
+print(lastRepeats(list9))
+print(lastRepeats(list10))
+print("\n\n")
+print("Least")
+print(least(list1))
+print(least(list2))
+print(least(list3))
+print(least(list4))
+print(least(list5))
+print(least(list6))
+print(least(list7))
+print(least(list8))
+print(least(list9))
+print(least(list10))
+print("\n\n")
+print(averageDropLow(list1))
+print(averageDropLow(list2))
+print(averageDropLow(list3))
+print(averageDropLow(list4))
+print(averageDropLow(list5))
+print(averageDropLow(list6))
+print(averageDropLow(list7))
+print(averageDropLow(list8))
+print(averageDropLow(list9))
+print(averageDropLow(list10))
+
+
+
+
+
+
+
+
+
+
